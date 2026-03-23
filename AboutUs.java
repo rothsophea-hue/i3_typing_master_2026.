48b3290e70e9069a51bb69fa867a066d87257675
powiney <powineyborin@gmail.com>
2026-03-23 19:31:47 +0700

    added  About Us screen

---
+++ AboutUs.java
@@ -0,0 +1,20 @@
+public class AboutUs {
+    public static void main(String[] args) {
+        System.out.println("--------------------------------------------------");
+        System.out.println("                 About Us Screen                  ");
+        System.out.println("--------------------------------------------------");
+        System.out.println("Welcome to I3 Typing Master v2026!");
+        System.out.println();
+        System.out.println("This project was developed by a team of students");
+        System.out.println("to help users practice and improve their typing speed.");
+        System.out.println();
+        System.out.println("Features include:");
+        System.out.println(" - User Registration and Login");
+        System.out.println(" - Typing Speed Test");
+        System.out.println(" - Test Result Tracking");
+        System.out.println(" - Friendly User Interface");
+        System.out.println();
+        System.out.println("Thank you for using our application!");
+        System.out.println("--------------------------------------------------");
+    }
+}
